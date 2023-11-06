f049dc499db0790ed12e39789586aa3c38c60657
Zurda13 <144481470+Zurda13@users.noreply.github.com>
2023-11-06 21:22:10 +0000

    proyecto domain bucle

---
+++ app.js
@@ -0,0 +1,14 @@
+let pronoun = ['the', 'our'];
+let adj = ['great', 'big'];
+let noun = ['jogger', 'racoon'];
+let web = [ '.com', '.net', '.us', '.io']
+
+for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+        for ( let k = 0; k < noun.length; k++) {
+            for ( let l = 0; l < web.length; l++) {
+                console.log(pronoun[i] + adj[j] + noun[k] + web[l]); 
+            }
+        }
+    }
+}
